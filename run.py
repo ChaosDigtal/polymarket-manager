f6ac32600afa7b6b21e5cfe8d35722be1781ddec
Tejas Mehta <digitalchaos2003@gmail.com>
2024-07-30 15:24:44 -0700

    v1-finish

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
-#from web3 import Web3
 import requests
 import os
 import asyncio
 from py_clob_client.client import ClobClient
-from py_clob_client.clob_types import ApiCreds, OrderArgs, OrderType, BalanceAllowanceParams, AssetType
+from py_clob_client.clob_types import OrderArgs, OrderType, BalanceAllowanceParams, AssetType
 from dotenv import load_dotenv
-from py_clob_client.constants import AMOY
 from datetime import datetime
 
 from py_clob_client.order_builder.constants import BUY
@@ -20,19 +18,11 @@ order_decicent_max_amount = 0.5 # Maximum order price for minimum_tick_size = 1/
 
 host = "https://clob.polymarket.com"
 key = os.getenv("PK")
+funder = os.getenv("FUNDER")
 chain_id = 137
-client = ClobClient(host, key=key, chain_id=chain_id, signature_type=2, funder="0xAd4ECa3bE320353ef0c36EdA7820Ef29431dec67")
+client = ClobClient(host, key=key, chain_id=chain_id, signature_type=2, funder=funder)
 creds = client.create_or_derive_api_creds()
 client.set_api_creds(creds=creds)
-
-def create_api_key():
-    host = "https://clob.polymarket.com"
-    key = os.getenv("PK")
-    chain_id = 137
-    #client = ClobClient(host, key=key, chain_id=chain_id)
-    client = ClobClient(host, key=key, chain_id=chain_id, signature_type=2, funder="0xAd4ECa3bE320353ef0c36EdA7820Ef29431dec67")
-
-    print(client.create_api_key())
     
 async def get_balance_allowance():
     collateral = client.get_balance_allowance(
@@ -43,15 +33,17 @@ async def get_balance_allowance():
     
 async def get_polymarket_markets(next_cursor=""):
     url = f"{host}/markets?next_cursor={next_cursor}"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
-        return data
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except Exception as err:
-        print(f"An error occurred: {err}")
+    while True:
+        try:
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()
+            data = response.json()
+            return data
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        except Exception as err:
+            print(f"An error occurred: {err}")
+        print("Retrying...")
 
 def create_order(price, size, token_id):
     order_args = OrderArgs(
@@ -67,7 +59,6 @@ def create_order(price, size, token_id):
     print(resp)
    
 async def parse_market(market):
-    market_id = market.get('condition_id', 'N/A')
     tokens = market.get('tokens', [])
     token_yes = tokens[0].get('token_id', 'N/A') if len(tokens) > 0 else 'N/A'
     token_no = tokens[1].get('token_id', 'N/A') if len(tokens) > 1 else 'N/A'
@@ -101,31 +92,38 @@ async def parse_market(market):
         cent += step
 
 async def main():
+    past_questionIDs = []
     next_cursor = "MTA1MDA="
-    data = None
     while True:
         data = await get_polymarket_markets(next_cursor)
+        for i in range(0, data["count"]):
+            market = data["data"][i]
+            if market.get('question', '').startswith('[Single Market]'):
+                continue
+            if not market.get('closed', True) and market.get('active', True) and market["tokens"][0]["token_id"] and market["tokens"][1]["token_id"]:
+                past_questionIDs.append(market["question_id"])
         if data["next_cursor"] == "LTE=":
             break
         next_cursor = data["next_cursor"]
-    current_count = data["count"]
-    print("Finding new markets...")
+    print("Finding markets...")
     while True:
         data = await get_polymarket_markets(next_cursor)
-        if data["count"] > current_count:
-            for i in range(current_count, data["count"]):
-                market = data["data"][i]
-                if market.get('question', '').startswith('[Single Market]'):
-                    continue
-                if not market.get('closed', True) and market.get('active', True):
+        filled = True
+        for i in range(0, data["count"]):
+            market = data["data"][i]
+            if market.get('question', '').startswith('[Single Market]'):
+                continue
+            if not market.get('closed', True) and market.get('active', True) and market["question_id"] not in past_questionIDs:
+                if market["tokens"][0]["token_id"] and market["tokens"][1]["token_id"]:
                     print("=====================")
                     print("New market found")
                     print(datetime.now().isoformat())
                     print(f"accepting_order_timestamp: {market["accepting_order_timestamp"]}")
+                    past_questionIDs.append(market["question_id"])
                     await parse_market(market)
-            current_count = data["count"]
-        if data["next_cursor"] != "LTE=":
-            current_count = 0
+                else:
+                    filled = False
+        if data["next_cursor"] != "LTE=" and filled:
             next_cursor = data["next_cursor"]
     
 asyncio.run(main())
