524e5df72400c5e707c7bd2a12d8b7b755a77946
Tejas Mehta <digitalchaos2003@gmail.com>
2024-09-20 06:59:26 -0700

    update-fok

--- new.py
+++ new.py
@@ -57,7 +57,6 @@ creds = client.create_or_derive_api_creds()
 client.set_api_creds(creds=creds)
 
 free_window_size = 0
-free_ws_position = 0
 
 def update_env(last_cursor):
     env_file_path = ".env"
@@ -145,22 +144,16 @@ async def get_portfolio_size():
     total_portfolio_size = total_position_size + balance_allowance
     return total_portfolio_size, balance_allowance
 
-async def create_buy_order(price, size, token_id, isAsk=False): # Place limit order, isAsk: True=> Entry 2, False=> Entry 1, return True if order is placed, False otherwise
+async def create_buy_order(price, size, token_id):
     try:
         order_args = OrderArgs(price=price,size=size,side=BUY,token_id=token_id)
         logger.info(f"Creating NO order: {order_args}")
         signed_order = client.create_order(order_args)
-        resp = client.post_order(signed_order, OrderType.GTC)
+        resp = client.post_order(signed_order, OrderType.FOK)
         logger.info(f"Order response: {resp}")
-        if isAsk and resp["status"] != "matched" and resp["orderID"]: # If Entry 2 and failed to purchase immediately
-            logger.info("Order Unmatched. Canceling...")
-            resp_cancel = client.cancel(order_id=resp["orderID"]) # Cancel the order since it failed to purchase immediately
-            if len(resp_cancel["canceled"]):
-                logger.info("Order Successfully Canceled.")
-                return False
-            else:
-                logger.info(f"Failed to Cancel Order due to {resp_cancel['not_canceled']}")
-                return True
+        if resp["errorMsg"] != "": # If failed to purchase immediately
+            logger.info("Order Unmatched.")
+            return False
         else:
             logger.info("Order Successfully Matched.")
             return True
@@ -173,18 +166,8 @@ async def create_sell_order(price, size, token_id):
         order_args = OrderArgs(price=price,size=size,side=SELL,token_id=token_id)
         signed_order = client.create_order(order_args)
         resp = client.post_order(signed_order, OrderType.GTC)
-        if resp["status"] != "matched" and resp["orderID"]: # Failed to purchase immediately
-            logger.info("Order Unmatched. Canceling...")
-            resp_cancel = client.cancel(order_id=resp["orderID"]) # Cancel the order since it failed to purchase immediately
-            if len(resp_cancel["canceled"]):
-                logger.info("Order Successfully Canceled.")
-                return False
-            else:
-                logger.info(f"Failed to Cancel Order due to {resp_cancel['not_canceled']}")
-                return True
-        else:
-            logger.info("Order Successfully Matched.")
-            return True
+        logger.info("Order Successfully Placed.")
+        return True
     except Exception as e:
         logger.error(f"Error creating SELL order: {e}")
         return False
@@ -196,6 +179,8 @@ async def cancel_orders_on_market(condition_id): # Cancel all open orders on mar
         )
     )
     for open_order in open_orders:
+        if open_order["side"] == "sell":
+            continue
         resp_cancel = client.cancel(order_id=open_order["id"])
         if len(resp_cancel["canceled"]):
             logger.info("Order Successfully Canceled.")
@@ -296,6 +281,9 @@ def add_priority(market_id):
             return True
     return False
 
+def truncate_to_2_decimals(value):
+    return int(value * 10) / 10.0
+
 async def monitor_market(market, portfolio_balance): # Monitor market
     global free_window_size
     if await check_resolved(market) == False:
@@ -344,8 +332,10 @@ async def monitor_market(market, portfolio_balance): # Monitor market
     if lowest_ask >= min_bid and lowest_ask <= max_bid and spread < spread_limit: # Entry Condition 2
         if await cancel_orders_on_market(market["condition_id"]): # Cancel current open orders
             limit_price = lowest_ask
-            if await create_buy_order(limit_price, available_balance / limit_price, market["no_asset_id"], True): # Place new order
+            size = truncate_to_2_decimals(available_balance / limit_price)
+            if await create_buy_order(limit_price, size, market["no_asset_id"]): # Place new order
                 add_priority(market["condition_id"])
+                await create_sell_order(SELL_TRESHOLD, size, market["no_asset_id"])
                 free_window_size += 1
                 return # If order is placed, return so that Entry 1 cannot place new order to ensure $ risked is under portfolio limit
     if is_in_watchlist(market) == False:
@@ -354,8 +344,10 @@ async def monitor_market(market, portfolio_balance): # Monitor market
     if highest_bid >= min_bid and highest_bid < max_bid: # Entry Conditoin 1
         if await cancel_orders_on_market(market["condition_id"]): # Cancel current open orders
             limit_price = highest_bid + min_tick_size
-            if await create_buy_order(limit_price, available_balance / limit_price, market["no_asset_id"]): # Place new order
+            size = truncate_to_2_decimals(available_balance / limit_price)
+            if await create_buy_order(limit_price, size, market["no_asset_id"]): # Place new order
                 add_priority(market["condition_id"])
+                await create_sell_order(SELL_TRESHOLD, size, market["no_asset_id"])
     free_window_size += 1
 
 async def monitor_active_markets():
@@ -464,78 +456,40 @@ async def initialize_markets_with_active_orders():
             pass
         time.sleep(1)
 
-async def check_position(market, shares):
-    global free_ws_position
-    try:
-        order_book = client.get_order_book(market["tokens"][1]["token_id"])
-    except Exception as e:
-        logger.info(f"order_book API error")
-        free_ws_position += 1
-        return
-    while len(order_book.bids) > 0 and shares > 0.0:
-        bid = order_book.bids.pop()
-        bid_price = float(bid.price)
-        bid_size = float(bid.size)
-        if bid_price < SELL_TRESHOLD:
-            break
-        sell_size = min(bid_size, shares)
-        if await create_sell_order(bid_price, sell_size, market["tokens"][1]["token_id"]) == True:
-            shares -= sell_size
-    free_ws_position += 1
-
 async def monitor_positions():
-    global free_ws_position
-    add_positions = True
-    while True:
-        trades = client.get_trades()
-        shares = dict()
-        for trade in trades:
-            if trade["status"] != "CONFIRMED":
-                continue
-            shares_bought = 0.0
-            if trade["maker_address"] == funder:
-                for maker_order in trade["maker_orders"]:
+    trades = client.get_trades()
+    shares = dict()
+    for trade in trades:
+        if trade["status"] != "CONFIRMED":
+            continue
+        shares_bought = 0.0
+        if trade["maker_address"] == funder:
+            for maker_order in trade["maker_orders"]:
+                shares_bought += float(maker_order["matched_amount"])
+        else:
+            for maker_order in trade["maker_orders"]:
+                if maker_order["maker_address"] == funder:
                     shares_bought += float(maker_order["matched_amount"])
-            else:
-                for maker_order in trade["maker_orders"]:
-                    if maker_order["maker_address"] == funder:
-                        shares_bought += float(maker_order["matched_amount"])
-            if shares_bought == 0.0:
-                continue
-            if trade["market"] in shares:
-                shares[trade["market"]] += shares_bought
-            else:
-                shares[trade["market"]] = shares_bought
-        
-        free_ws_position = 5
-        
-        for market, share in shares.items():
-            market = client.get_market(market)
-            if market["active"] == True and market["closed"] == False and market["tokens"][1]["outcome"] == "No" and market["tokens"][0]["winner"] == False and market["tokens"][1]["winner"] == False:
-                if add_positions == True:
-                    if add_priority(market["condition_id"]) == False:
-                        active_markets.append({
-                            "condition_id": market["condition_id"],
-                            "no_asset_id": market["tokens"][1]["token_id"],
-                            "min_tick_size": float(market["minimum_tick_size"]),
-                            "start_iso_date": market["accepting_order_timestamp"].replace("2024", "2050")
-                        })
-                        while len(active_markets) > WATCHLIST_LIMIT:
-                            active_markets.pop(0)
-                while free_ws_position == 0:
-                    pass
-                new_thread = threading.Thread(target=run_check_position, args=(market, share,))
-                free_ws_position -= 1
-                new_thread.start()
-                
-        while free_ws_position != 5:
-            pass
-        
-        add_positions = False
-        time.sleep(10)
-
-def run_check_position(market, shares):
-    asyncio.run(check_position(market, shares))
+        if shares_bought == 0.0:
+            continue
+        if trade["market"] in shares:
+            shares[trade["market"]] += shares_bought
+        else:
+            shares[trade["market"]] = shares_bought
+            
+    for market, share in shares.items():
+        market = client.get_market(market)
+        if market["active"] == True and market["closed"] == False and market["tokens"][1]["outcome"] == "No" and market["tokens"][0]["winner"] == False and market["tokens"][1]["winner"] == False:
+            if add_priority(market["condition_id"]) == False:
+                active_markets.append({
+                    "condition_id": market["condition_id"],
+                    "no_asset_id": market["tokens"][1]["token_id"],
+                    "min_tick_size": float(market["minimum_tick_size"]),
+                    "start_iso_date": market["accepting_order_timestamp"].replace("2024", "2050")
+                })
+                while len(active_markets) > WATCHLIST_LIMIT:
+                    active_markets.pop(0)
+            await create_sell_order(SELL_TRESHOLD, truncate_to_2_decimals(share), market["tokens"][1]["token_id"])
 
 def run_monitor_positions():
     asyncio.run(monitor_positions())
